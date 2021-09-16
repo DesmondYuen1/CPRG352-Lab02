46c4e6a810c55af30f7f20615577336d4ce172d3
user <user@DESKTOP-FLHNA44.cg.shawcable.net>
2021-09-16 16:52:56 -0600

    Added one line of code Print the text "Hello World!"

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,7 +16,8 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String text = "Hello World";
+        String text = "Hello World!";
+        System.out.print(text);
     }
     
 }
